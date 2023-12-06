71c4f05021e64e03c71bd640fb0ac1e6c61f73b7
Xoaquin Castrelo Brignell <xoaquin.cb@gmail.com>
2023-12-06 01:05:51 +0000

    Modify network layer API. Enables more efficient use of memory when passing data between layers.

--- include/network_stack/net.h
+++ include/network_stack/net.h
@@ -4,52 +4,88 @@
 #include <stdint.h>
 
 /**
- * Represents a network layer address.
+ * A network address. Can be any value between 0-255 (inclusive).
  */
-typedef uint8_t net_address_t;
+typedef uint8_t net_address;
 
 /**
- * Represents the checksum mode used on a network packet.
+ * The type of checksum generated on a network packet.
  */
 typedef enum {
     NET_CHECKSUM_NONE = 0b00,
     NET_CHECKSUM_EVEN_PARITY = 0b01,
-} net_checksum_mode_t;
+} net_checksum_type;
 
 /**
- * @brief Callback function pointer for receiving a network packet.
+ * @brief A callback function pointer for handling a received network data packet.
  * @param source: The source address that the packet came from.
- * @param data: A pointer to the data received. Note that after this function returns this pointer is no longer valid,
- *              so if the data is needed after the function returns it must be copied to a separate buffer.
- * @param data_length: The number of bytes of data. This will not be larger than 121.
+ * @param payload: A pointer to the first byte in the packet's payload. Note that this pointer is only valid until the
+ *                 function returns, so if the data is needed for longer it must be copied to a separate buffer. This
+ *                 pointer will be 'NULL' if and only if 'length' is zero.
+ * @param length: The number of bytes in the payload.
  */
-typedef void (*net_receive_callback_t)(net_address_t source, const uint8_t *data, uint8_t data_length);
+typedef void (*net_receive_callback)(net_address source, uint8_t *payload, uint8_t length);
 
 /**
- * @brief Initialises the network layer.
- * @param own_address: The network address that this device should use.
+ * @brief Initialises the network layer. Must be called once at the start of the program before calling any other
+ *        'net_()' functions.
+ * @param own_address: The network address that this device should use. This should be picked carefully to avoid
+ *                     multiple devices on the same network from having the same address.
+ * @param checksum_type: The type of checksum to generate on all outgoing packets.
  */
-void net_initialise(net_address_t own_address);
+void net_initialise(net_address own_address, net_checksum_type checksum_type);
 
 /**
- * @brief Returns whether a node is connected to the network.
- * @param node: The node's address.
+ * @brief Updates the network layer logic. This should be called periodically.
  */
-bool net_is_node_online(net_address_t address);
+void net_update();
 
 /**
- * @brief Sets the user function to be called when a packet is received.
- * @param callback: The function to call. Set to 'NULL' to use no callback.
+ * @brief Returns this device's network address, as set by the 'net_initialise()' function.
+ * @returns The network address.
  */
-void net_receive_packet_callback(net_receive_callback_t callback);
+net_address net_get_own_address();
 
 /**
- * @brief Attempts to send a packet to the specified network address.
- * @param destination: The address to send the packet to.
- * @param checksum_mode: The type of checksum to generate on the packet.
- * @param data: A pointer to the data to send.
- * @param data_length: The number of bytes of data. Must not be larger than 121.
- * @return 'true' if the packet was send; 'false' if there was some error. Note that this doesn't confirm that the
- *         destination received the packet.
+ * @brief Returns whether a given device is  known to be connected to the network.
+ * @param address: The device's network address.
+ * @returns 'true' if the device is online or 'false' otherwise.
  */
-bool net_send_packet(net_address_t destination, net_checksum_mode_t checksum_mode, const uint8_t *data, uint8_t data_length);
+bool net_is_device_online(net_address address);
+
+/**
+ * @brief Sets the user function to be called when a network data packet is received.
+ * @param callback: The function to be called. Set to 'NULL' to not use a callback (default).
+ */
+void net_set_receive_callback(net_receive_callback callback);
+
+/**
+ * @brief Allocates memory for a data buffer inside a network packet. Any data can be placed in the returned buffer, and
+ *        it can be sent with 'net_send_data_buffer()'. If after creating a buffer, it no longer needs to be sent, it
+ *        must instead be discarded with 'net_discard_data_buffer()' to free up the memory.
+ * @param length: How long to make the buffer, in bytes. Buffers with zero length or longer than 121 bytes will always
+ *                fail to be allocated.
+ * @returns A pointer to the allocated buffer, or 'NULL' if it couldn't be allocated. If allocation fails, it should be
+ *          tried again later when memory may have become available.
+ */
+uint8_t *net_create_data_buffer(uint8_t length);
+
+/**
+ * @brief Sends out a data buffer to the specified network address and then discards the buffer.
+ * @param buffer: The buffer to send. This must be a buffer that was allocated with 'net_create_data_buffer()'; using a
+ *                buffer from anywhere else will result in undefined behaviour. Once sent, the buffer is automatically
+ *                discarded and must not be used again. Passing a value of 'NULL' will have no effect and nothing will
+ *                be sent.
+ * @param destination: The address to send the data to.
+ */
+void net_send_data_buffer(uint8_t *buffer, net_address destination);
+
+/**
+ * @brief Discards a data buffer without sending it, freeing up the memory for new buffers.
+ * @param buffer: The buffer to discard. This must be a buffer that was allocated with 'net_create_data_buffer()'; using
+ *                a buffer from anywhere else will result in undefined behaviour. Once discarded, the buffer becomes
+ *                invalid and must not be used again. Passing a value of 'NULL' will have no effect.
+ * @note A buffer should only be discarded if it has NOT been sent with 'net_send_data_buffer()' since
+ *       'net_send_data_buffer()' already automatically discards packets.
+ */
+void net_discard_data_buffer(uint8_t *buffer);
