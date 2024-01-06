a14b1144f3763a73473df2fa90430a1f253848c1
Xoaquin Castrelo Brignell <xoaquin.cb@gmail.com>
2024-01-06 19:12:48 +0000

    Add another checksum function.

--- source/network_stack/net/checksum.c
+++ source/network_stack/net/checksum.c
@@ -2,22 +2,47 @@
 
 uint16_t net_generate_checksum(net_checksum_type checksum_type, uint8_t *data, uint8_t length) {
     switch (checksum_type) {
-
         case NET_CHECKSUM_EVEN_PARITY: {
             uint8_t parity_bit = 0;
+            // For each byte in the data:
             for (uint8_t byte_i = 0; byte_i != length; byte_i++) {
                 uint8_t current_byte = data[byte_i];
+                // For each bit in the byte:
                 for (uint8_t bit_i = 0; bit_i < 8; bit_i++) {
+                    // Update the parity bit:
                     parity_bit ^= (current_byte & 0x01);
+
+                    // Move to the next bit:
                     current_byte >>= 1;
                 }
             }
+            // Return the parity bit in the least-significant bit of the checksum:
             return (uint16_t) parity_bit;
         } break;
 
         case NET_CHECKSUM_NONE:
         default:
+            // No checksum to generate. Checksum should be zero:
             return 0;
+    }
+}
 
+bool net_fix_checksum_errors(net_checksum_type checksum_type, uint16_t checksum, uint8_t *data, uint8_t data_length) {
+    // Currently there are no checksum types that actually fix any errors, but there could be in the future.
+
+    switch (checksum_type) {
+        case NET_CHECKSUM_EVEN_PARITY: {
+            // Generate checksum and compare against expected checksum:
+            uint16_t generated_checksum = net_generate_checksum(NET_CHECKSUM_EVEN_PARITY, data, data_length);
+            return generated_checksum == checksum;
+        } break;
+
+        case NET_CHECKSUM_NONE:
+            // No checksum to generate; checksum check should always pass:
+            return true;
+
+        default:
+            // Unknown checksum type; checksum check should pass automatically:
+            return true;
     }
 }
--- source/network_stack/net/checksum.h
+++ source/network_stack/net/checksum.h
@@ -1,12 +1,38 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
 
-// The type of checksum generated on a network packet.
+/**
+ * The default checksum type that should be used on packets sent by this device.
+ */
+#define NET_TX_CHECKSUM_TYPE (NET_CHECKSUM_EVEN_PARITY)
+
+/**
+ * The type of checksum generated on a network packet.
+ */
 typedef enum {
     NET_CHECKSUM_NONE = 0b00,
     NET_CHECKSUM_EVEN_PARITY = 0b01,
 } net_checksum_type;
 
-// Generates a given checksum type on some data.
+/**
+ * @brief Generates a checksum on some data.
+ * @param checksum_type: The type of checksum to generate.
+ * @param data: A pointer to the start of the data to generate a checksum on.
+ * @param data_length: The number of bytes of data.
+ * @returns The 16-bit checksum generated.
+ */
 uint16_t net_generate_checksum(net_checksum_type checksum_type, uint8_t *data, uint8_t data_length);
+
+/**
+ * @brief Generates a checksum on some data, and then compares it against the expected checksum. If the type of checksum
+ *        being used is able to correct errors in the data, it does so.
+ * @param checksum_type: The type of checksum to use.
+ * @param expected_checksum: The data's expected checksum.
+ * @param data: A pointer to the start of the data.
+ * @param data_length: The number of bytes of data.
+ * @returns 'true' if there are no errors in the data, or any errors were fixed; 'false' if there are uncorrected errors
+ *          remaining in the data.
+ */
+bool net_fix_checksum_errors(net_checksum_type checksum_type, uint16_t expected_checksum, uint8_t *data, uint8_t data_length);
